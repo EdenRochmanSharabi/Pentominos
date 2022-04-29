7bb7ee9cf76378a3c6c8ed5fa7ee8fc55db3ea87
EdenRochmanSharabi <91745090+EdenRochmanSharabi@users.noreply.github.com>
2022-04-29 14:34:47 +0200

    Project 1-1 2021/2021

---
+++ knapsackproblem/mutationrateMutator.java
@@ -0,0 +1,11 @@
+public class mutationrateMutator {
+    private static double mutationrate;
+
+    public mutationrateMutator(double mutationrate){
+        mutationrateMutator.mutationrate = mutationrate;
+    }
+    public static double getMutation(){
+        if(mutationrate<100) mutationrate++;
+        return mutationrate;
+    }
+}
